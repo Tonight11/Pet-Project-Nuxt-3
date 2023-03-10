1f86d6a29d0b0a16ddf10c51f08ba79f1b9c01e1
tonight11 <godofspeedman@gmail.com>
2023-03-10 20:06:13 +0300

    refactor: meta lesson path

--- server/api/meta.get.ts
+++ server/api/meta.get.ts
@@ -10,7 +10,9 @@ const lessonSelect = Prisma.validator<Prisma.LessonArgs>()({
 	},
 });
 
-export type LessonNeeded = Prisma.LessonGetPayload<typeof lessonSelect>;
+export type LessonNeeded = Prisma.LessonGetPayload<typeof lessonSelect> & {
+	path: string;
+};
 
 const chapterSelect = Prisma.validator<Prisma.ChapterArgs>()({
 	select: {
@@ -21,8 +23,13 @@ const chapterSelect = Prisma.validator<Prisma.ChapterArgs>()({
 	},
 });
 
-export type ChapterNeeded = Prisma.ChapterGetPayload<typeof chapterSelect>;
-
+// export type ChapterNeeded = Prisma.ChapterGetPayload<typeof chapterSelect>;
+export type ChapterNeeded = Omit<
+	Prisma.ChapterGetPayload<typeof chapterSelect>,
+	'lessons'
+> & {
+	lessons: LessonNeeded[];
+};
 const courseSelect = Prisma.validator<Prisma.CourseArgs>()({
 	select: {
 		title: true,
@@ -30,8 +37,39 @@ const courseSelect = Prisma.validator<Prisma.CourseArgs>()({
 	},
 });
 
-export type CourseNeeded = Prisma.CourseGetPayload<typeof courseSelect>;
+// export type CourseNeeded = Prisma.CourseGetPayload<typeof courseSelect>;
+export type CourseNeeded = Omit<
+	Prisma.CourseGetPayload<typeof courseSelect>,
+	'chapters'
+> & {
+	chapters: ChapterNeeded[];
+};
 
-export default defineEventHandler(() => {
-	return prisma.course.findFirst(courseSelect);
+export default defineEventHandler(async () => {
+	const courseNeeded = await prisma.course.findFirst(courseSelect);
+
+	if (!courseNeeded) {
+		throw createError({
+			statusCode: 404,
+			statusMessage: 'Course not found',
+		});
+	}
+
+	const chapters = courseNeeded.chapters.map(chapter => {
+		const lesson = chapter.lessons.map(lesson => {
+			return {
+				...lesson,
+				path: `/course/chapter/${chapter.slug}/lesson/${lesson.slug}`,
+			};
+		});
+		return {
+			...chapter,
+			lesson,
+		};
+	});
+	console.log(chapters);
+	return {
+		...courseNeeded,
+		chapters,
+	};
 });
