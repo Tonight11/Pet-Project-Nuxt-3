d6f1a7acc4a8757aa0281f59a9ff784dddc60abe
tonight11 <godofspeedman@gmail.com>
2023-03-11 12:38:09 +0300

    feat: endPoint for lessonProgress

---
+++ server/api/course/chapter/[chapterSlug]/lesson/[lessonSlug]/progress.ts
@@ -0,0 +1,58 @@
+import { PrismaClient } from '@prisma/client';
+import { serverSupabaseUser } from '#supabase/server';
+
+const prisma = new PrismaClient();
+
+export default defineEventHandler(async event => {
+	assertMethod(event, ['POST', 'PATCH', 'PUT']);
+
+	const user = await serverSupabaseUser(event);
+	if (!user) {
+		throw createError({
+			statusCode: 401,
+			message: 'Unauthorized',
+		});
+	}
+
+	const { lessonSlug, chapterSlug } = event.context.params;
+
+	const lesson = await prisma.lesson.findFirst({
+		where: {
+			slug: lessonSlug,
+			Chapter: {
+				slug: chapterSlug,
+			},
+		},
+	});
+
+	if (!lesson) {
+		throw createError({
+			statusCode: 404,
+			message: 'Lesson not found',
+		});
+	}
+
+	const { completed } = await readBody(event);
+	const email = user.email as string;
+
+	prisma.progressLesson.upsert({
+		where: {
+			lessonId_userEmail: {
+				userEmail: email,
+				lessonId: lesson.id,
+			},
+		},
+		update: {
+			completed,
+		},
+		create: {
+			completed,
+			userEmail: email,
+			lesson: {
+				connect: {
+					id: lesson.id,
+				},
+			},
+		},
+	});
+});
