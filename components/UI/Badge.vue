543cb230255e83c7ea1843e31698116aa702c633
tonight11 <godofspeedman@gmail.com>
2023-03-14 21:13:16 +0300

    feat: UI components

---
+++ components/UI/Badge.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+	interface Type {
+		color: string;
+	}
+	withDefaults(defineProps<Type>(), {
+		color: 'bg-blue-600',
+	});
+</script>
+
+<template>
+	<span
+		class="-left-12 text-xl w-8 h-8 text-white rounded-full inline-flex absolute items-center justify-center mr-4"
+		:class="`${color}`"
+	>
+		<slot>&#10003;</slot>
+	</span>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ components/UI/Section.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+	interface Title {
+		title?: string;
+	}
+	defineProps<Title>();
+</script>
+
+<script lang="ts">
+	export default {
+		inheritAttrs: false,
+	};
+</script>
+
+<template>
+	<div class="section bg-white p-24 flex flex-col items-center">
+		<div class="w-full max-w-4xl">
+			<h2 v-if="title" class="text-4xl font-bold pb-8 text-center">
+				{{ title }}
+			</h2>
+			<div v-bind="$attrs">
+				<slot />
+			</div>
+		</div>
+	</div>
+</template>
+
+<style lang="scss">
+	.section:nth-of-type(even) {
+		@apply bg-blue-500 text-white;
+	}
+</style>
