8375f058199c32980c8362451c0f0f9a332f6b9d
tonight11 <godofspeedman@gmail.com>
2023-03-15 20:00:14 +0300

    feat: prerender laning page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	nitro: {
+		prerender: {
+			routes: ['/landing'],
+		},
+	},
 	modules: [
 		'@nuxtjs/tailwindcss',
 		'@vueuse/nuxt',
