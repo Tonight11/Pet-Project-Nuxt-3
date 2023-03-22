f23d328f7b6d601327c963d570b2c5dd3b864700
tonight11 <godofspeedman@gmail.com>
2023-03-22 21:31:00 +0300

    feat: creating coursePay data on db

--- server/api/stripe/payment.post.ts
+++ server/api/stripe/payment.post.ts
@@ -1,13 +1,41 @@
+import { PrismaClient } from '@prisma/client';
 import Stripe from './stripe';
+
+const prisma = new PrismaClient();
+
 export default defineEventHandler(async event => {
 	const { email } = await readBody(event);
+	let response;
 
-	const response = await Stripe.paymentIntents.create({
-		amount: 97 * 100,
-		currency: 'usd',
-		metadata: {
-			email,
-		},
-	});
+	try {
+		response = await Stripe.paymentIntents.create({
+			amount: 97 * 100,
+			currency: 'usd',
+			metadata: {
+				email,
+			},
+		});
+	} catch (e) {
+		throw createError({
+			statusCode: 500,
+			statusMessage: 'Error! Try later or text to support',
+		});
+	}
+
+	try {
+		await prisma.coursePayment.create({
+			data: {
+				email: email,
+				paymentId: response.id,
+				courseId: 1,
+			},
+		});
+	} catch (e) {
+		throw createError({
+			statusCode: 500,
+			statusMessage: 'Error! Try later or text to support',
+		});
+	}
+
 	return response.client_secret;
 });
