01eda3b2a60ad10d9441c3840a7829f6051ac0bc
tonight11 <godofspeedman@gmail.com>
2023-03-23 20:07:09 +0300

    feat: webook endpoint

---
+++ server/api/stripe/webhook.pots.ts
@@ -0,0 +1,69 @@
+import { PrismaClient } from '@prisma/client';
+import Stripe from './stripe';
+
+const prisma = new PrismaClient();
+const secretKey = useRuntimeConfig().secretKey;
+
+interface PaymentIntent {
+	id: string;
+}
+
+export default defineEventHandler(async event => {
+	const signature = getHeader(event, 'stripe-signature');
+	const body = await readRawBody(event);
+
+	let stripeEvent;
+	try {
+		stripeEvent = await Stripe.webhooks.constructEvent({
+			body,
+			signature,
+			secretKey,
+		});
+	} catch (e) {
+		throw createError({
+			statusCode: 500,
+			statusMessage: 'Error! Try later or text to support',
+		});
+	}
+
+	if (stripeEvent.type === 'payment_intent.succeeded') {
+		await handlePaymentIntentSucceeded(stripeEvent.data.object);
+	} else if (stripeEvent.type === 'payment_intent.payment_failed') {
+		await handlePaymentIntentFailed(stripeEvent.data.object);
+	}
+
+	return 200;
+});
+
+const handlePaymentIntentSucceeded = async (paymentIntent: PaymentIntent) => {
+	try {
+		await prisma.coursePayment.update({
+			where: {
+				paymentId: paymentIntent.id,
+			},
+			data: {
+				verifed: true,
+			},
+		});
+	} catch (e) {
+		throw createError({
+			statusCode: 500,
+			statusMessage: 'Error verifying purchase',
+		});
+	}
+};
+
+const handlePaymentIntentFailed = async (paymentIntent: PaymentIntent) => {
+	try {
+		await prisma.coursePayment.delete({
+			where: {
+				paymentId: paymentIntent.id,
+			},
+		});
+	} catch (e) {
+		throw createError({
+			statusCode: 500,
+			statusMessage: 'Error removing purchase',
+		});
+	}
+};
